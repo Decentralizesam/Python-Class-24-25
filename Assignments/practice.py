deb74b4949d84a42e17b9ad6950303fd6d510ef5
Decentralizesam <decentralizebrand@gmail.com>
2024-05-23 13:13:10 +0300

    added work

---
+++ Assignments/practice.py
@@ -0,0 +1,127 @@
+: ARITHEMETIC OPERATORS EXAMPLE
+
+# Example 1 
+x = 5
+y = 3
+
+print(x + y)
+
+# Example 2
+x = 5
+y = 3
+
+print(x + y)
+
+# Example 3
+
+
+x = 10
+y = 5
+
+print(x - y)
+
+# Example 4
+
+w = 100
+z = 50
+
+print(w * z)
+
+# Example 5
+
+x = 10
+y = 2
+
+print(x / y)
+
+# Example 6
+
+a = 6
+b = 10
+
+print(a % b)
+
+
+ : LOGICAL OPERATORS EXAMPLE
+
+# Example 1
+
+x = 5
+
+print(x > 3 and x < 10)
+
+# Returns True because (5) is greater than (3 & 5) is less than (10)
+
+# Example 2
+
+x = 5
+
+print(x > 3 or x < 4)
+
+# Returns True because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+
+# Example 3
+
+x = 5
+
+print(not(x > 3 and x < 10))
+
+# Returns False because not is used to reverse the result, so not(5>3 and 5<10) is False
+
+# Example 4
+
+x = 2
+
+print(x > 1 and x < 5)
+
+# Returns True because both conditions are true (2 is greater than 1, and 2 is less than 5)
+
+# Example 5
+
+x = 2
+
+print(x > 1 or x < 4)
+
+# Returns True because one of the conditions are true (2 is greater than 1, but 2 is not less than 4)
+
+
+: INEQUALITY OPERATORS EXAMPLE
+
+# In Python, the 'not equal' operator is represented by '! ='.
+# It's a comparison operator used to compare two values.
+ #The operator returns True if the values are not equal and False if they are equal.
+
+ # Example 1 
+
+ x = "10"
+ y = 10
+
+ print(x!=y)
+
+ # returns true because x is not equal to y
+
+ #Example 2
+
+ x = 5
+y = 3
+
+print(x == y)
+
+# returns False because 5 is not equal to 3
+
+
+:ASSIGNMENT OPERATORS EXAMPLE
+
+# Example 1
+
+x = 5
+
+print(x)
+
+# Example 2
+
+x = 5
+
+x += 3
+
+print(x)
