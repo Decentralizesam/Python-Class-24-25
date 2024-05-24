f989e04376cb71dc9fed72e5758e6d72f012890e
Decentralizesam <decentralizebrand@gmail.com>
2024-05-24 18:00:00 +0300

    addedwork

---
+++ Phython/Dictionaries/dict.py
@@ -0,0 +1,21 @@
+dict1={'a': 1, 'b': 2, 'c': 3}
+dict1['b'] =4
+
+dict_update({'a':1, 'b':4 , 'c':6})# update dict 'c'
+print(dict1)
+
+del dict1['a'] # remove key word a 
+
+
+print(numbers) 
+print (dict1['b'])
+get_dict=dict1.get('a')
+print (get_dict)
+
+dict_keys=dict1.keys()# get dict keys only
+print (dict_keys)
+
+dict_values=dict1.values()# get dict values only
+print (dict_values)
+
+# dictionaries and list should be in finger tipes
---
+++ Phython/Tuples/tuples2.py
@@ -0,0 +1,6 @@
+tup1=(1,2,3,4,5)
+tup2=(6,7,8,9)
+
+tup3=tup1+tup2
+for element in tup3:
+    print(element)
